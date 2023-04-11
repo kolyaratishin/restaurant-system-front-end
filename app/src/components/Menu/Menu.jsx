c5fd49fd7bb1c6448138fb3d9c287cc6f1246e3e
kolyaratishin <kolyaratishin1@gmail.com>
2023-04-11 15:10:52 +0300

    changed styles of menu page

--- app/src/components/Menu/Menu.jsx
+++ app/src/components/Menu/Menu.jsx
@@ -4,13 +4,13 @@ import MenuForm from "./MenuForm/MenuForm";
 
 function Menu(props) {
 
-    const addMealToMenu = (values) =>{
+    const addMealToMenu = (values) => {
         props.addMealToMenu(values);
     }
 
     let menu = props.menu;
     return (
-        <div>
+        <div className={classes.menu_content}>
             <div>
                 <table className={classes.menu_table}>
                     <caption>Меню</caption>
@@ -22,10 +22,13 @@ function Menu(props) {
                     {menu.map(item => <MenuItem name={item.name} price={item.price} size={item.size}/>)}
                 </table>
             </div>
-            <div>
+            <div className={classes.menu_form}>
+                <div className={classes.form_caption}>
+                    ДОДАВАННЯ НОВОГО ПРОДУКТУ
+                </div>
                 <MenuForm onSubmit={addMealToMenu}/>
             </div>
-            <div>
+            <div className={classes.import_export}>
                 Import/Export
             </div>
         </div>
--- app/src/components/Menu/MenuForm/MenuForm.jsx
+++ app/src/components/Menu/MenuForm/MenuForm.jsx
@@ -1,15 +1,19 @@
 import {Field, reduxForm} from "redux-form";
+import classes from "./MenuForm.module.css";
 
 
 function MenuForm(props) {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field component={"textarea"} name="name" placeholder="Введіть ім'я продукту"/>
-                <Field component={"textarea"} name="price" placeholder="Введіть ціну продукту"/>
-                <Field component={"textarea"} name="size" placeholder="Введіть розмір продукту"/>
+                <p className={classes.input_name}>Назва</p>
+                <Field component={"input"} name="name" placeholder="Введіть ім'я продукту"/>
+                <p className={classes.input_name}>Ціна</p>
+                <Field component={"input"} name="price" placeholder="Введіть ціну продукту"/>
+                <p className={classes.input_name}>Розмір</p>
+                <Field component={"input"} name="size" placeholder="Введіть розмір продукту"/>
             </div>
-            <button>Додати</button>
+            <button className={classes.add_button}>Додати</button>
         </form>
     );
 }
