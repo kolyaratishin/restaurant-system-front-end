f45503327b803553e3ae33c4876f72e077de8a63
kolyaratishin <kolyaratishin1@gmail.com>
2023-04-14 14:17:16 +0300

    changed initial state for table reducer

--- app/src/state/table-reducer.js
+++ app/src/state/table-reducer.js
@@ -4,10 +4,7 @@ const SET_TABLES = "SET_TABLES"
 
 let initialState = {
     tables: [
-        {id: 1, name: "Стіл 1", status: "FREE"},
-        {id: 2, name: "Стіл 2", status: "FREE"},
-        {id: 3, name: "Стіл 3", status: "FREE"},
-        {id: 4, name: "Стіл 4", status: "FREE"},
+        
     ]
 }
 
