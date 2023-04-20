fe70bc90b0fb92a0d2a4e5d6aac97af75bf34d0d
kolyaratishin <kolyaratishin1@gmail.com>
2023-04-20 11:56:00 +0300

    added styles for login form

--- app/src/components/Auth/Login/Login.jsx
+++ app/src/components/Auth/Login/Login.jsx
@@ -1,13 +1,13 @@
-import classes from "../../Menu/AddGroupForm/AddGroupForm.module.css";
+import classes from "./Login.module.css";
 import {Field, reduxForm} from "redux-form";
 import {withRouter} from "../../../hoc/withRouter";
 
 function Login(props) {
 
     return (
-       <div>
-           <LoginFormRedux login={props.login}/>
-       </div>
+        <div>
+            <LoginFormRedux login={props.login}/>
+        </div>
     );
 }
 
@@ -18,15 +18,25 @@ function LoginForm(props) {
     }
 
     return (
-        <form onSubmit={props.handleSubmit(onLoginSubmit)}>
-            <div>
-                <p className={classes.input_name}>Логін</p>
-                <Field component={"input"} name="username" placeholder="Введіть логін" type={"text"}/>
-                <p className={classes.input_name}>Пароль</p>
-                <Field component={"input"} name="password" placeholder="Введіть пароль" type={"password"}/>
-            </div>
-            <button className={classes.add_button}>Увійти</button>
-        </form>
+        <div className={classes.form_container}>
+            <form className={classes.login_form} onSubmit={props.handleSubmit(onLoginSubmit)}>
+                <h2>Авторизація</h2>
+                <div>
+                    <div className={classes.form_group}>
+                        <p className={classes.input_name}>Логін</p>
+                        <Field component={"input"} name="username" placeholder="Введіть логін" type={"text"}/>
+                    </div>
+                    <div className={classes.form_group}>
+                        <p className={classes.input_name}>Пароль</p>
+                        <Field component={"input"} name="password" placeholder="Введіть пароль" type={"password"}/>
+                    </div>
+                </div>
+                <div className={classes.form_group}>
+                    <button className={classes.login_button}>Увійти</button>
+                </div>
+            </form>
+        </div>
+
     );
 }
 
