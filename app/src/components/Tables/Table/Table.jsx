e31127470f837d999942d5da2c3f35d1d63b77a5
kolyaratishin <kolyaratishin1@gmail.com>
2023-04-27 11:33:22 +0300

    changed displaying status

--- app/src/components/Tables/Table/Table.jsx
+++ app/src/components/Tables/Table/Table.jsx
@@ -1,11 +1,15 @@
 import classes from "./Table.module.css";
 
 function Table(props) {
+    const status = props.status === "FREE" ? "Вільно" : props.status === "BUSY" ? "Зайнято" : "Замовлено";
+
     return (
         <div className={classes.table}>
-            {props.name}
+            <div className={classes.table_name}>
+                {props.name}
+            </div>
             <div>
-                Status: {props.status}
+                Статус: {status}
             </div>
         </div>
     );
